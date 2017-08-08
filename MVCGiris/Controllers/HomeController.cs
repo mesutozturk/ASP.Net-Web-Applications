37ac7fb068cb94d4db17137bfdc48e8dba8b1074
Mesut OZTURK <mesutoztrk@msn.com>
2017-08-08 10:35:33 +0300

    added first mcv controller and action!

---
+++ MVCGiris/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCGiris.Models;
+
+namespace MVCGiris.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var urunler= Urun.Urunler;
+            return View(urunler);
+        }
+        [HttpPost]
+        public ActionResult Index(int a)
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCGiris/Models/Urun.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCGiris.Models
+{
+    public class Urun
+    {
+        public int id { get; set; }
+        public string UrunAdi { get; set; }
+        public decimal Fiyat { get; set; }
+
+        public static List<Urun> Urunler
+        {
+            get
+            {
+                return new List<Urun>(){
+                    new Urun()
+                    {
+                        id = 1,
+                        Fiyat = 5,
+                        UrunAdi = "Kola"
+                    },
+                    new Urun()
+                    {
+                        id = 2,
+                        Fiyat = 9,
+                        UrunAdi = "Tavuk Döner"
+                    },
+                    new Urun()
+                    {
+                        id = 3,
+                        Fiyat = 14,
+                        UrunAdi = "Dana Döner"
+                    }};
+            }
+        }
+
+    }
+}
