83a5147308ba5d4adbd60f253ae48e95571f39ef
Mesut OZTURK <mesutoztrk@msn.com>
2017-08-08 13:38:18 +0300

    added controllers and actions

---
+++ North/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using North.Models;
+
+namespace North.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var kategoriler = new NorthwindEntities().Categories.ToList();
+            return View(kategoriler);
+        }
+    }
+}
---
+++ North/Controllers/KategoriController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using North.Models;
+
+namespace North.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        public ActionResult Urunler(int? id)
+        {
+            if (id == null)
+                return RedirectToAction("Index", "Home");
+            var kategori = new NorthwindEntities().Categories.Find(id);
+            if (kategori != null)
+            {
+                var urunler = kategori.Products.ToList();
+                return View(urunler);
+            }
+            return View(new List<Product>());
+        }
+    }
+}
