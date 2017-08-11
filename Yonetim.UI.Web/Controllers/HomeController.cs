8a2e3c5868a2b1ec739065afd174b00819c190ca
mesutozturk <mesutoztrk@msn.com>
2017-08-11 10:15:57 +0300

    added main admin layout and partial results

---
+++ Yonetim.UI.Web/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Yonetim.UI.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        #region PartialResults
+
+        public PartialViewResult NavHeaderPartial()
+        {
+            return PartialView("_NavHeaderPartial");
+        }
+
+        public PartialViewResult NavRightPartial()
+        {
+            return PartialView("_NavbarRightPartial");
+        }
+
+        public PartialViewResult SideBarPartial()
+        {
+            return PartialView("_SideBarPartial");
+        }
+        
+        #endregion    
+    }
+}
