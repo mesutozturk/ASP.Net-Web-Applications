5e9dc818c7043e8b7e1672f7731164fe60202b94
mesutozturk <mesutoztrk@msn.com>
2017-08-16 13:57:18 +0300

    added angularjs

---
+++ WebApiGiris/Scripts/appcontroller.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="angular.js"/>
+/// <reference path="appmodule.js"/>
+
+app.controller("categoryCtrl",
+    function ($scope,api) {
+        $scope.ad = "Kamil";
+        $scope.kategoriler = [];
+        $scope.getir = function () {
+            api.getallcategories(function(response) {
+                $scope.kategoriler = response;
+                console.log(response);
+            });
+        }
+    });
---
+++ WebApiGiris/Scripts/appmodule.js
@@ -0,0 +1,18 @@
+﻿/// <reference path="angular.js"/>
+var app = angular.module("northapp", []);
+
+app.factory("api", function ($http) {
+    var apiurl = "http://localhost:2148/api/";
+    return {
+        getallcategories: function (success) {
+            $http({
+                url: apiurl + 'Category',
+                method: 'GET',
+                dataType: 'JSON'
+            }).then(function(response) {
+                success(response.data);
+            });
+        }
+    }
+
+});
